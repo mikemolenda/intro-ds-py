d3727d1649514d4d4209eb9f2ada3bbdd0ac5dee
Mike Molenda <molenda.mike@gmail.com>
2017-06-13 16:22:23 -0500

    Remove format strings from print statements

--- lectures/week1/types_sequences.py
+++ lectures/week1/types_sequences.py
@@ -6,14 +6,14 @@ print('Check Types:')
 def my_func():
     return None
 
-print('type(\'string\'):\t{}'.format(str(type('string'))))
-print('type(None):\t{}'.format(str(type(None))))
-print('type(1):\t{}'.format(str(type(1))))
-print('type(1.0):\t{}'.format(str(type(1.0))))
-print('type(my_func):\t{}'.format(str(type(my_func))))
-print('type([1, 2, 3]:\t{}'.format(str(type([1, 2, 3]))))
-print('type((1, 2, 3):\t{}'.format(str(type((1, 2, 3)))))
-print('type(\{1, 2, 3\}:\t{}'.format(str(type({1, 2, 3}))))
+print(str(type('string')))
+print(str(type(None)))
+print(str(type(1)))
+print(str(type(1.0)))
+print(str(type(my_func)))
+print(str(type([1, 2, 3])))
+print(str(type((1, 2, 3))))
+print(str(type({1, 2, 3})))
 
 print()
 
