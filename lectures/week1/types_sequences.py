bd7a34fb73a8792f9b4dbcb7f3c02895749bdbda
Mike Molenda <molenda.mike@gmail.com>
2017-06-13 16:20:22 -0500

    Add escape characters before curly brace literals in format string

--- lectures/week1/types_sequences.py
+++ lectures/week1/types_sequences.py
@@ -13,7 +13,7 @@ print('type(1.0):\t{}'.format(str(type(1.0))))
 print('type(my_func):\t{}'.format(str(type(my_func))))
 print('type([1, 2, 3]:\t{}'.format(str(type([1, 2, 3]))))
 print('type((1, 2, 3):\t{}'.format(str(type((1, 2, 3)))))
-print('type({1, 2, 3}:\t{}'.format(str(type({1, 2, 3}))))
+print('type(\{1, 2, 3\}:\t{}'.format(str(type({1, 2, 3}))))
 
 print()
 
