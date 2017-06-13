5e671bad975c44bb30ad05752ca2e657c96451cd
Mike Molenda <molenda.mike@gmail.com>
2017-06-13 16:16:04 -0500

    Start conversion of types_sequences.ipynb to pure Python. Complete through Check Types section

---
+++ lectures/week1/types_sequences.py
@@ -0,0 +1,23 @@
+#! /usr/local/bin/python3
+
+# Check Types
+print('Check Types:')
+
+def my_func():
+    return None
+
+print('type(\'string\'):\t{}'.format(str(type('string'))))
+print('type(None):\t{}'.format(str(type(None))))
+print('type(1):\t{}'.format(str(type(1))))
+print('type(1.0):\t{}'.format(str(type(1.0))))
+print('type(my_func):\t{}'.format(str(type(my_func))))
+print('type([1, 2, 3]:\t{}'.format(str(type([1, 2, 3]))))
+print('type((1, 2, 3):\t{}'.format(str(type((1, 2, 3)))))
+print('type({1, 2, 3}:\t{}'.format(str(type({1, 2, 3}))))
+
+print()
+
+
+# Sequences: Iterate and Index
+print('Sequences: Iterate and Index')
+
