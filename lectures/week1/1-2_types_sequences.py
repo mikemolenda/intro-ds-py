2a309e069c0a4e8e9e140afab1d43ad6e59c4d20
Mike Molenda <molenda.mike@gmail.com>
2017-06-14 07:02:36 -0500

    Add lesson number to pure python files. Remove functions Jupyter notebook.

---
+++ lectures/week1/1-2_types_sequences.py
@@ -0,0 +1,22 @@
+#! /usr/local/bin/python3
+
+# Check Types
+print('Check Types:')
+
+def my_func():
+    return None
+
+print(str(type('string')))
+print(str(type(None)))
+print(str(type(1)))
+print(str(type(1.0)))
+print(str(type(my_func)))
+print(str(type([1, 2, 3])))
+print(str(type((1, 2, 3))))
+print(str(type({1, 2, 3})))
+
+print()
+
+
+# Sequences: Iterate and Index
+print('Sequences: Iterate and Index')
