f9f9a4a39ae8b12503a8087287fcb0705b0447b3
Mike Molenda <molenda.mike@gmail.com>
2017-06-16 07:55:37 -0500

    Complete lesson 2-1 part 1 (creating Series), start part 2 (querying series)

--- lectures/week2/2-1_pandas_series_data_structure.py
+++ lectures/week2/2-1_pandas_series_data_structure.py
@@ -3,6 +3,8 @@
 import pandas as pd
 import numpy as np
 
+# PART 1 CREATE SERIES
+
 # Pandas Series are collections with index, type and name
 # Numpy arrays used to store data under the hood
 animals = ['Tiger', 'Bear', 'Moose']
@@ -21,4 +23,29 @@ print(pd.Series(animals))
 print()
 print(pd.Series(numbers))
 print()
+
+# Convert dictionary to Pandas Series
+sports = {'Archery': 'Bhutan',
+          'Golf': 'Scotland',
+          'Sumo': 'Japan',
+          'Taekwondo': 'South Korea'}
+
+sports_series = pd.Series(sports)
+
+print(sports)
+print()
+print(sports_series)
+print()
+print(sports_series.index)
+print()
+
+# Explicitly assign index values to series
+animals_series = pd.Series(['Tiger', 'Bear', 'Moose'], index=['India', 'America', 'Canada'])
+
+print(animals_series)
+print()
+
+
+# PART 2 QUERY SERIES
+
 
