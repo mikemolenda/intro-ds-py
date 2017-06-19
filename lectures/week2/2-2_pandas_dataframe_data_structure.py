215b8c0dd8a7c1ae6d8ce9aca8ddae6e3dadbf27
Mike Molenda <molenda.mike@gmail.com>
2017-06-18 22:10:58 -0500

    Add data set to lesson 2

--- lectures/week2/2-2_pandas_dataframe_data_structure.py
+++ lectures/week2/2-2_pandas_dataframe_data_structure.py
@@ -3,3 +3,21 @@
 import pandas as pd
 import numpy as np
 
+# Create from dictionaries (dict = row)
+
+purchase_1 = pd.Series({'Name': 'Chris',
+                        'Item Purchased': 'Dog Food',
+                        'Cost': 22.5})
+purchase_2 = pd.Series({'Name': 'Kevyn',
+                        'Item Purchased': 'Kitty Litter',
+                        'Cost': 2.5})
+purchase_3 = pd.Series({'Name': 'Vinod',
+                        'Item Purchased': 'Bird Seed',
+                        'Cost': 5.0})
+
+pet_supplies = pd.DataFrame([purchase_1, purchase_2, purchase_3], index=['Store 1', 'Store 1', 'Store 2'])
+
+print(pet_supplies)
+print()
+
+
