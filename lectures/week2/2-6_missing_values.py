08a3dac50ade0c7fc54e5b887dd96705955a0b54
Mike Molenda <molenda.mike@gmail.com>
2017-06-20 15:37:00 -0500

    Complete lesson 6 Missing Values

--- lectures/week2/2-6_missing_values.py
+++ lectures/week2/2-6_missing_values.py
@@ -5,3 +5,23 @@ import numpy as np
 
 # Handling missing values
 
+player_log = pd.read_csv('../../data/log.csv')
+
+print(player_log)
+print()
+
+# Use fillna('ffill') to forward-fill NaN values from previous occupied row - be sure to index/sort first!
+player_log.set_index(['time', 'user'], inplace=True)
+
+# One column only
+fill_paused = player_log.copy()
+fill_paused['paused'].fillna(method='ffill', inplace=True)
+
+print(fill_paused)
+print()
+
+# All columns
+player_log.fillna(method='ffill', inplace=True)
+
+print(player_log)
+print()
